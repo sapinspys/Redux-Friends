80733b17c7410b0b7b6584d4e883cf6e0646cce6
Guillermo Arria-Devoe <garri009@fiu.edu>
2019-03-06 15:34:42 -0500

    Imported components into App

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,10 +1,16 @@
 import React, { Component } from 'react';
+
+import AddFriendFrom from './components/AddFriendForm';
+import Friends from './components/Friends';
+
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <Friends />
+        <AddFriendFrom />
       </div>
     );
   }
