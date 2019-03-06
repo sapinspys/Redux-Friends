09ed61cfe914235d2a4173fb2a97e28b299eef28
Guillermo Arria-Devoe <garri009@fiu.edu>
2019-03-06 16:05:15 -0500

    Injected store into App using Provider and included middleware

--- friends/src/index.js
+++ friends/src/index.js
@@ -14,9 +14,14 @@ import rootReducer from './reducers';
 const store = createStore(
   rootReducer,
   applyMiddleware(thunk, logger),
-)
+);
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>, 
+  document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
