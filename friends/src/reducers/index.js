3414abe7eddbddd8edf84eea5d88c274113bd46e
Guillermo Arria-Devoe <garri009@fiu.edu>
2019-03-06 15:51:06 -0500

    Constructed first reducer to fetch friends

---
+++ friends/src/reducers/index.js
@@ -0,0 +1,31 @@
+import { FETCH_FRIENDS, SUCCESS, FAILURE } from "../actions";
+
+const initialState = {
+  fetchingFriends: false,
+  friends: [],
+  error: null,
+}
+
+export const fetchingReducer = (state = initialState, action) => {
+  switch(action.typpes) {
+    case FETCH_FRIENDS:
+      return {
+        ...state,
+        fetchingFriends: true,
+      }
+    case SUCCESS:
+      return {
+        ...state,
+        fetchingFriends: false,
+        friends: action.payload,
+      }
+    case FAILURE:
+      return {
+        ...state,
+        error: action.payload,
+        friends: [],
+      }
+    default:
+      return state;
+  }
+}
