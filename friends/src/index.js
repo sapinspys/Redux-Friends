5034cc5e76de500415c98f43fbc486c8ee999789
Guillermo Arria-Devoe <garri009@fiu.edu>
2019-03-06 16:03:16 -0500

    Created store using rootReducer

--- friends/src/index.js
+++ friends/src/index.js
@@ -4,6 +4,18 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+import { Provider } from "react-redux";
+import { applyMiddleware, createStore } from "redux";
+import thunk from 'redux-thunk';
+import logger from 'redux-logger';
+
+import rootReducer from './reducers';
+
+const store = createStore(
+  rootReducer,
+  applyMiddleware(thunk, logger),
+)
+
 ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
