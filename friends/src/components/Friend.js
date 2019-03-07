ee84b89389d4871a87e67602962111e57eef5534
Guillermo Arria-Devoe <garri009@fiu.edu>
2019-03-06 23:25:46 -0500

    Styled cards further

--- friends/src/components/Friend.js
+++ friends/src/components/Friend.js
@@ -4,15 +4,26 @@ import { Card } from 'reactstrap';
 
 // INLINE STYLING
 const card = {
-  width: '225px',
+  width: '240px',
+  marginBottom: '20px',
+  background: 'whitesmoke',
+  border: 'none',
+  boxShadow: '0 0 5px black',
+}
+
+const cardTitle = {
+  paddingBottom: '5px', 
+  borderBottom: '1px solid silver', 
+  background: 'silver',
+  padding: '5px 0',
 }
 
 const Friend = props => {
   return (
     <Card style={card}>
-      <h3>{props.friend.name}</h3>
-      <p>Age: {props.friend.age}</p>
-      <p>Email: {props.friend.email}</p>
+      <h3 style={cardTitle}>{props.friend.name}</h3>
+      <p><strong>Age</strong>:<br />{props.friend.age}</p>
+      <p><strong>Email</strong>:<br />{props.friend.email}</p>
     </Card>
   )
 }
