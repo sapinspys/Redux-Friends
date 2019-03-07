e4a1d5f2597916fa0f994f4eacb6f864ec893748
Guillermo Arria-Devoe <garri009@fiu.edu>
2019-03-07 15:06:48 -0500

    Built login page component

--- friends/src/components/Login.js
+++ friends/src/components/Login.js
@@ -1,17 +1,53 @@
 import React, { Component } from 'react';
 
 class Login extends Component {
+  state = {
+    credentials: {
+      username: '',
+      password: ''
+    }
+  }
+
+  login = e => {
+    e.preventDefault();
+    props.login(this.state.credentials)
+      .then(() => {
+        // only fires if login call is successful
+        this.props.history.push('/');
+      })
+  }
+
+  handleChange = e => {
+    this.setState({
+      credentials: {
+        ...this.state.credentials,
+        [e.target.name]: e.target.value,
+      }
+    });
+  }
 
   render() {
     return (
       <div>
-        <header>
-          <h1>Login Page Coming Soon!</h1>
-        </header>
+        <form onSubmit={this.login}>
+          <input
+            type="text"
+            name="username"
+            value={this.state.credentials.username}
+            onChange={this.handleChange}
+          />
+          <input
+            type="password"
+            name="password"
+            value={this.state.credentials.password}
+            onChange={this.handleChange}
+          />
+          <button>Log in</button>
+        </form>
       </div>
     );
   }
 }
 
-export default Login;
+export default connect(null, { login })(Login);
 
